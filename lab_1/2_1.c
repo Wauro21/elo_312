57febd70595f9015578ca84bbfedd4ae325f2e92
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-09-26 17:51:23 -0300

    Update 2_1.c

--- lab_1/2_1.c
+++ lab_1/2_1.c
@@ -1,27 +1,34 @@
 
-#include "io430.h"
 #include <stdio.h>
+
 int numbers[20]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}; //Declaracion variable global
 int number_to_find = 7; //Numero a buscar
 
-int main( void )
+void search(int to_find, int n_array[] )
 {
   //declaracion variables locales
   int cont = 0 ; //Contador
   while (cont <= 19)
   {
-    if (numbers[cont] == number_to_find)
+    if (n_array[cont] == to_find)
     {
       printf("El numero se encontro en el indice: %d",cont);
       break;
     }
     else if (cont == 19)
     {
-      printf("No se hayo el numero: %d",number_to_find);
+      printf("No se hayo el numero: %d",to_find);
+      cont++;
     }
     else 
     {
       cont++; //Se incremente el contador
     }
   }
+  return;
+}
+
+void main(void)
+{
+  search(number_to_find,numbers);
 }
