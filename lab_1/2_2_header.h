51b7f63c129b21bb9dc1964caf1947d18829b7c6
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-09-26 22:30:33 -0300

    Create 2_2_header.h

---
+++ lab_1/2_2_header.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCION_
+#define _FUNCION_
+unsigned int multiply_pow_2_v1 (unsigned int number, unsigned int power);
+unsigned int multiply_pow_2_v2 (unsigned int number, unsigned int power);
+void comparar ( unsigned int number, unsigned int power);
+#endif
