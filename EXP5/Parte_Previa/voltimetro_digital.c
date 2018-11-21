5b249fe0ce615518b5f86a7e0f4666f41f269c01
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-11-21 15:59:17 -0300

    Update voltimetro_digital.c

--- EXP5/Parte_Previa/voltimetro_digital.c
+++ EXP5/Parte_Previa/voltimetro_digital.c
@@ -4,7 +4,7 @@
 #include "display.h"
 #include "osc.h"
 #include "adc.h"
-
+int *d;
 void main(void)
 {
     osc_init_xt2();
@@ -17,7 +17,8 @@ void main(void)
 
     TACTL = TASSEL_1 + MC_1;    // reloj ACLK, modo up to CCR0
 
-    adc_init_single_channel_single_conv();
+    adc_init_single_channel_single_conv_it();
+	*d =adc_single_channel_single_conv_it(0);
 
     _BIS_SR(LPM0_bits + GIE);	// modo bajo consumo
 
@@ -27,29 +28,26 @@ void main(void)
 
 #define CONV(x)      (3,3/4095,0)*x			// convertir x en voltaje
 // Timer A0 rutina de servicio de interrupción
-#pragma vector = TIMERA0_VECTOR
-__interrupt void Timer_A (void)
-{
-	int pos = display_get_pos();
-	int d;
-	d = adc_single_channel_single_conv(0);
-	printf("Vol=%1.3f\n", CONV(d));
-	display_set_pos(pos);
-}
+//#pragma vector = TIMERA0_VECTOR
+//__interrupt void Timer_A (void)
+//{
+//	int pos = display_get_pos();
+//	int d;
+//	d = adc_single_channel_single_conv(0);
+//	printf("Vol=%1.3f\n", CONV(d));
+//	display_set_pos(pos);
+//}
 
-#pragma vector = TIMERB0_VECTOR
-__interrupt void Timer_B (void)
-{
+//#pragma vector = TIMERB0_VECTOR
+//__interrupt void Timer_B (void)
+//{
 
-}
-#
+//}
 
 #pragma vector = ADC12_VECTOR
 __interrupt void ADC(void) //Cuando se escribe un valor en la memoria de ADC, se llama a la rutina y se envia el resultado a la pantalla
 {
 	int pos = display_get_pos();
-	int d;
-	d = adc_single_channel_single_conv(0);
-	printf("Vol=%1.3f\n", CONV(d));
+	printf("Vol=%1.3f\n", CONV(*d));
 	display_set_pos(pos);  
 }
