b3a7d297c7c6c0f12eaaf03ab8975a40de266ef3
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-12-13 20:41:44 -0300

    Update i2c.c

--- proyecto__final/i2c.c
+++ proyecto__final/i2c.c
@@ -1,16 +1,17 @@
 // Driver I2C
 // -----> Includes    <------ //
+#include <msp430x16x.h>
 #include "i2c.h"
 // -----> Definitions <----- //
-#define IC2_SCL_PIN BIT3
-#define IC2_SDA_PIN BIT1
+#define I2C_SCL_PIN BIT3
+#define I2C_SDA_PIN BIT1
 
 
 void i2c_init(unsigned int slave_address, int i2c_mode)
 {
   P3SEL |= I2C_SCL_PIN | I2C_SDA_PIN;
   U0CTL |= I2C + SYNC; //Inicialiazacion
-  UOCTL &= ~I2CEN; // Apaga modulo I2C, para modificar registros
+  U0CTL &= ~I2CEN; // Apaga modulo I2C, para modificar registros
   I2CTCTL |= I2CSSEL1; //Reloj SMCLK
   I2CIE = RXRDYIE | TXRDYIE; // Interrupciones reception/transmision
   if(i2c_mode)
@@ -22,5 +23,6 @@ void i2c_init(unsigned int slave_address, int i2c_mode)
   {
     I2COA = slave_address; //Direccion propia
   }
+  
   U0CTL |= I2CEN; //Activa modulo IC2
 }
