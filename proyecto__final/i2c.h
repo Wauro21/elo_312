e92724b0075e287689fd09a27291bfcacfc61fea
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-12-13 20:18:44 -0300

    Create i2c.h

---
+++ proyecto__final/i2c.h
@@ -0,0 +1,13 @@
+#ifndef __I2C__H
+#define __I2C__H
+
+// ----> Definiciones <---- //
+
+#define I2C_MASTER 0x01
+#define I2C_SLAVE  0x00
+
+// ---> Funciones < ---- //
+
+void i2c_init(unsigned int slave_address, int i2c_mode)
+
+#endif
