a4be386fc91fd0ebe38a7ea9f8f7001bb11a8b54
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-12-13 23:14:07 -0300

    Update i2c.c

--- proyecto__final/i2c.c
+++ proyecto__final/i2c.c
@@ -3,10 +3,10 @@
 #include <msp430x16x.h>
 #include "i2c.h"
 // -----> Definitions <----- //
-#define I2C_SCL_PIN BIT3
+#define I2C_SCL_PIN BIT3while();
 #define I2C_SDA_PIN BIT1
 
-
+//Funcion incializacion I2C
 void i2c_init(unsigned int slave_address, int i2c_mode)
 {
   P3SEL |= I2C_SCL_PIN | I2C_SDA_PIN;
@@ -25,4 +25,22 @@ void i2c_init(unsigned int slave_address, int i2c_mode)
   }
   
   U0CTL |= I2CEN; //Activa modulo IC2
+}
+
+// Master read
+unsigned int master_reads(unsigned int read_address)
+{
+  U0CTL &= ~I2CEN; //Permite modificar ciertos registros
+  I2CNDAT = 1 ; //Tamano direccion a leer
+  U0CTL |= MST; //Modo maestro
+  UOCTL |= I2CEN;
+  
+  //Inicio transferencia
+  I2CTCTL |= I2CSTT + I2CTRX; // Condicion start y modo transmision maestro
+  while(!(IC2DCTL & I2CBUSY)); 
+  I2CIFG &= ~ARDYIFG;
+  while((I2CIFG & TXRDYIFG) == 0);
+  I2CDRB = read_address;
+  while(!(I2CIFG & ARDYIFG));
+  // Se lee la información
 }
