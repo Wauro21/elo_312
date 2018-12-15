bfdbcf0c242d2e06f878d5b2e89f4e864fa131b1
RMardonesT <45024705+RMardonesT@users.noreply.github.com>
2018-12-15 10:47:56 -0300

    Create utils.h

---
+++ proyecto__final/utils.h
@@ -0,0 +1,11 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+typedef unsigned char HAL_StatusTypeDef;
+typedef unsigned char uint8_t;
+typedef signed int int16_t;
+typedef unsigned int uint16_t;
+typedef unsigned long int uint32_t;
+
+void m_delay(uint16_t limit);
+#endif
