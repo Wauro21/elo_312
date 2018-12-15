e21ee142b5f0788fd2ae1afa0e19685db2830d9c
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-12-14 21:30:20 -0300

    Update i2c.c

--- proyecto__final/i2c.c
+++ proyecto__final/i2c.c
@@ -3,9 +3,9 @@
 #include <msp430x16x.h>
 #include "i2c.h"
 // -----> Definitions <----- //
-#define I2C_SCL_PIN BIT3while();
+#define I2C_SCL_PIN BIT
 #define I2C_SDA_PIN BIT1
-
+#define BYTE_NUMBER
 //Funcion incializacion I2C
 void i2c_init(unsigned int slave_address, int i2c_mode)
 {
@@ -30,17 +30,31 @@ void i2c_init(unsigned int slave_address, int i2c_mode)
 // Master read
 unsigned int master_reads(unsigned int read_address)
 {
+  unsigned int data = 0x00;
   U0CTL &= ~I2CEN; //Permite modificar ciertos registros
-  I2CNDAT = 1 ; //Tamano direccion a leer
+  I2CNDAT = 0x01 ; //Tamano direccion a leer
   U0CTL |= MST; //Modo maestro
   UOCTL |= I2CEN;
   
   //Inicio transferencia
   I2CTCTL |= I2CSTT + I2CTRX; // Condicion start y modo transmision maestro
-  while(!(IC2DCTL & I2CBUSY)); 
+  while(!(IC2DCTL & I2CBUSY)); // 
   I2CIFG &= ~ARDYIFG;
   while((I2CIFG & TXRDYIFG) == 0);
   I2CDRB = read_address;
   while(!(I2CIFG & ARDYIFG));
   // Se lee la información
+  I2CNDAT = BYTE_NUMBER;
+  U0CTL |= MST; //Se apaga después de una condicion de stop
+  I2CTCTL &= !I2CTRX; //Modo recepcion
+  I2CTCTL |= I2CSTT + I2CSTP;
+  while(!(IC2DCTL & I2CBUSY));
+  for(counter = 0; counter < BYTE_NUMBER; counter++)
+  {
+    while((I2CIFG & RXRDYIFG) == 0);
+    data[counter] = I2CDRB;
+  }
+  while(I2CTCTL & I2CSTP);
+  return data;
+  
 }
