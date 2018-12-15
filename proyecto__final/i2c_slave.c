c0bd3d66d6d4f0e3f28f47955321afa1298a7421
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2018-12-15 10:42:00 -0300

    Update and rename i2c.c to i2c_slave.c

--- proyecto__final/i2c_slave.c
+++ proyecto__final/i2c_slave.c
@@ -8,8 +8,8 @@
 #define I2C_SDA_PIN BIT1
 #define BYTE_NUMBER 0x02
 // ----> Variables utiles <---- //
-static unsigned int PWM[2];
-static unsigned int pwm_counter=0x00;
+//static unsigned int PWM[2];
+static unsigned int pwm_counter=0;
 //Funcion incializacion I2C
 void i2c_init(unsigned int slave_address, int i2c_mode)
 {
@@ -17,7 +17,7 @@ void i2c_init(unsigned int slave_address, int i2c_mode)
   U0CTL |= I2C + SYNC; //Inicialiazacion
   U0CTL &= ~I2CEN; // Apaga modulo I2C, para modificar registros
   I2CTCTL |= I2CSSEL1; //Reloj SMCLK
-  I2CIE = RXRDYIE | TXRDYIE; // Interrupciones reception/transmision
+  I2CIE = TXRDYIE | RXRDYIE; // Interrupciones reception/transmision
   if(i2c_mode)
   {
     I2CNDAT = 0x02; //Cantidad de bytes transferidos
@@ -26,27 +26,36 @@ void i2c_init(unsigned int slave_address, int i2c_mode)
   else
   {
     I2COA = slave_address; //Direccion propia
+    //I2CDCTL &= ~I2CRXOVR;
   }
   
   U0CTL |= I2CEN; //Activa modulo IC2
 }
 
 //Slave read
-unsigned int* slave_reads(unsigned int* save)
+void slave_reads(unsigned int* save)
 {
-  unsigned int count = 0x00;
-  for(count = 0x00; (count < 6); count++)
+  unsigned int count = 0;
+  for(count =0; count <3; count++)
   {
-    if(I2CDRB%3 != 0)
-    {
-      if((count == 0x01) | (count == 0x04))
-      {
-        *(PWM+count) = (I2CDRB<<8);
-      }
-      else
-      {
-        *(PWM + count) |= I2CDRB;
-      }
-    }
+    *(save + count) = I2CDRB;
+     }
   }
-}
+    //  while(!((I2CIFG & RXRDYIFG)==RXRDYIFG));
+//  unsigned int count = 0x00;
+//  for(count = 0x00; (count < 6); count++)
+//  {
+//    if(I2CDRB%3 != 0)
+//    {
+//      if((count == 0x01) | (count == 0x04))
+//      {
+//        *(save+count-1) = (I2CDRB<<8);
+//      }
+//      else
+//      {
+//        *(save + count-1) |= I2CDRB;
+//      }
+//    }
+//  }
+//}
+
